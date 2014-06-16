81d63ad67e3679ff8d0425b9c6cd5c8eeef24b8d
Travis Briggs <audiodude@gmail.com>
2014-06-16 02:50:42 +0000

    Working with bcrypt for hashing password

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sqlite3'
+require 'bcrypt'
 
 get '/' do 
   erb :index
@@ -12,10 +13,11 @@ end
 post '/register' do
   db = SQLite3::Database.new 'user_login.sqlite'
   
-  # TODO: Only allow each username once.
+  # TODO: Only allow each username once
+  hashed_password = BCrypt::Password.create(params[:password])
   
   db.execute('INSERT INTO users (email, username, password) VALUES (?, ?, ?)',
-     [params[:email], params[:username], params[:password]])
+    [params[:email], params[:username], hashed_password])
   redirect '/'
 end
 
@@ -25,9 +27,15 @@ end
 
 post '/login' do
   db = SQLite3::Database.new 'user_login.sqlite'
-  users = db.execute('SELECT id FROM users WHERE username = ? AND password = ? LIMIT 1',
-    [params[:username], params[:password]])  
+  users = db.execute('SELECT id, password FROM users WHERE username = ? LIMIT 1', [params[:username]])  
+  # [ [1, 'abc544334fdd001'] ]
   if users.size == 0
+    @error = 'No user with that username or password found!'
+    return erb :login
+  end
+
+  hashed_password = BCrypt::Password.new(users[0][1])
+  if hashed_password != params[:password]
     @error = 'No user with that username or password found!'
     return erb :login
   end
